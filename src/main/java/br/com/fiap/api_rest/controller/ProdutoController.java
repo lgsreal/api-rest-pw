81bca260a688156c5107e641a42b0b2ea2d1d205
lgsreal <lgsreal2@gmail.com>
2026-04-08 20:22:58 -0300

    Swagger

--- src/main/java/br/com/fiap/api_rest/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/api_rest/controller/ProdutoController.java
@@ -5,6 +5,12 @@ import br.com.fiap.api_rest.dto.ProdutoRequest;
 import br.com.fiap.api_rest.dto.ProdutoResponse;
 import br.com.fiap.api_rest.model.Produto;
 import br.com.fiap.api_rest.service.ProdutoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -20,6 +26,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/produtos")
+@Tag(name = "api-produtos")
 public class ProdutoController {
     private final ProdutoService produtoService;
 
@@ -27,12 +34,14 @@ public class ProdutoController {
         this.produtoService = produtoService;
     }
 
+    @Operation(summary = "Cria um novo produto")
     @PostMapping
     public ResponseEntity<Produto> createProduto(@Valid @RequestBody ProdutoRequest produto) {
         Produto produtoSalvo = produtoService.create(produto);
         return new ResponseEntity<>(produtoSalvo, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Busca um produto por id")
     @GetMapping("/{id}")
     public ResponseEntity<ProdutoResponse> readProduto(@PathVariable UUID id) {
         ProdutoResponse produto = produtoService.read(id);
@@ -48,6 +57,19 @@ public class ProdutoController {
     // HATEOAS
     // PageAnterior: localhost:8080/produtos?pageNumber=0
     // PageSeguinte: null
+    @Operation(summary = "Busca produtos por página")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                            description = "Página de produtos retornada com sucesso!",
+                            content = @Content(mediaType = "application/json",
+                                                schema = @Schema(implementation = ProdutoLista.class)
+                            )
+            ),
+            @ApiResponse(responseCode = "404",
+                            description = "Nenhum produto encontrado",
+                            content = @Content(schema = @Schema())
+            )
+    })
     @GetMapping
     public ResponseEntity<Page<ProdutoLista>> readProduto(@RequestParam(defaultValue = "0") Integer pageNumber) {
         // page number, page size, sort
@@ -59,6 +81,7 @@ public class ProdutoController {
         return new ResponseEntity<>(produtos, HttpStatus.OK);
     }
 
+    @Operation(summary = "Atualiza um produto específico")
     @PutMapping
     public ResponseEntity<Produto> updateProduto(@RequestBody Produto produto) {
         ProdutoResponse produtoExistente = produtoService.read(produto.getId());
@@ -69,6 +92,7 @@ public class ProdutoController {
         return new ResponseEntity<>(produtoAtualizado, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Exclui um produto por id")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduto(@PathVariable UUID id) {
         produtoService.delete(id);
